6854cfa95f25fe546b09e7a2bbe8eec914c2d1a7
Liza <lizka4231@gmail.com>
2025-11-18 16:25:47 +0300

    feat(client MainLayout): add MainLayout, add routing

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { type FC } from 'react';
+
+import MainLayout from '../router/MainLayout';
+
+const Home: FC = () => {
+   return (
+      <MainLayout>
+         home
+      </MainLayout>
+   )
+}
+
+export default Home;
---
+++ client/src/router/App.tsx
@@ -0,0 +1,16 @@
+import { type FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+
+const App: FC = () => {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Home />} />
+         </Routes>
+      </BrowserRouter>
+   )
+}
+
+export default App;
