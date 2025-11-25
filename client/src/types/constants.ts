61910339e07220a0951f8be8b93d8564908cc0aa
lizka <lizka4231@gmail.com>
2025-11-25 20:51:17 +0300

    feat(constants): add weekdays

--- client/src/types/constants.ts
+++ client/src/types/constants.ts
@@ -3,3 +3,13 @@ export const roomTypeOptions = [
    { value: 'practice', label: 'Для практик' },
    { value: 'universal', label: 'Универсальная' }
 ];
+
+export const weekDays = [
+   { label: 'Понедельник', value: 'monday' },
+   { label: 'Вторник', value: 'tuesday' },
+   { label: 'Среда', value: 'wednesday' },
+   { label: 'Четверг', value: 'thursday' },
+   { label: 'Пятница', value: 'friday' },
+   { label: 'Суббота', value: 'saturday' },
+   { label: 'Воскресенье', value: 'sunday' },
+];
