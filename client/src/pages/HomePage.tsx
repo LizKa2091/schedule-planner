ab763d6eaa650eaa5831837794d409618f00ebc2
lizka <lizka4231@gmail.com>
2025-11-29 15:07:02 +0300

    feat(nearest-lesson): add UI for nearest lesson, add it into HomePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,30 @@
 import { type FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Flex } from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+
+import NearestLesson from '@/components/nearest-lesson/NearestLesson';
+
+dayjs.locale('ru');
 
 const HomePage: FC = () => {
+   const navigate = useNavigate();
+
+   const handleClick = () => {
+      navigate('/manage');
+   }
+
    return (
-      <p>home</p>
+      <Flex vertical align='center' gap='large'>
+         <h1>Добрый день!</h1>
+         <h2>Сегодня {dayjs().format('D MMMM YYYY года')}</h2>
+         <Flex vertical align='center' justify='center' gap='middle'>
+            <h3>Ближайшие занятия: </h3>
+            <NearestLesson />
+         </Flex>
+         <Button type='primary' onClick={handleClick}>Перейти к управлению расписанием</Button>
+      </Flex>
    )
 }
 
